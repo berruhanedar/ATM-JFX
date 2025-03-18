c479282afed39423db1c43546c0ad599c063b629
berru <berruhanedar@icloud.com>
2025-03-19 00:01:45 +0300

    update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -12,6 +12,7 @@ module com.berru.app.atmjfx {
     requires java.sql;
     requires java.desktop;
     requires eu.hansolo.tilesfx;
+    requires com.h2database;
 
     opens com.berru.app.atmjfx to javafx.fxml;
     exports com.berru.app.atmjfx;
