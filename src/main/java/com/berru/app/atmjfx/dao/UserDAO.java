5d585ebbd3f87f60dba40d7ef2df81a16230bdb4
berru <berruhanedar@icloud.com>
2025-03-22 17:25:49 +0300

    Implement CRUD operations in UserDAO class using SingletonDBConnection

--- src/main/java/com/berru/app/atmjfx/dao/UserDAO.java
+++ src/main/java/com/berru/app/atmjfx/dao/UserDAO.java
@@ -1,20 +1,76 @@
 package com.berru.app.atmjfx.dao;
 
+import com.berru.app.atmjfx.database.SingletonDBConnection;
 import com.berru.app.atmjfx.dto.UserDTO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class UserDAO implements IDaoImplements<UserDTO>{
+public class UserDAO implements IDaoImplements<UserDTO> {
+
+    // Field
+    private Connection connection;
+
+    public UserDAO(Connection connection) {
+        this.connection = SingletonDBConnection.getInstance().getConnection();
+    }
+
+    /////////////////////////////////////
+    // CRUD
 
     @Override
     public Optional<UserDTO> create(UserDTO userDTO) {
+        String sql = "INSERT INTO users (username , password , email) VALUES (?, ?, ?)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, userDTO.getUsername());
+            preparedStatement.setString(2, userDTO.getPassword());
+            preparedStatement.setString(3, userDTO.getEmail());
+
+            int affectedRows = preparedStatement.executeUpdate();
+
+            if (affectedRows > 0) {
+                try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
+                    if (generatedKeys.next()) {
+                        userDTO.setId(generatedKeys.getInt(1)); // OTomatik ID set et
+                        return Optional.of(userDTO);
+                    }
+                } catch (SQLException sqlException) {
+                    sqlException.printStackTrace();
+                }
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        // Eğer Ekleme başarısızsa boş veri dönder
         return Optional.empty();
     }
 
     @Override
-    public List<UserDTO> list() {
-        return List.of();
+    public Optional<List<UserDTO>> list() {
+        List<UserDTO> userDTOList = new ArrayList<>();
+        String sql = "SELECT * FROM users";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            ResultSet resultSet = preparedStatement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                userDTOList.add(UserDTO.builder()
+                        .id(resultSet.getInt("id"))
+                        .username(resultSet.getString("username"))
+                        .password(resultSet.getString("password"))
+                        .email(resultSet.getString("email"))
+                        .build());
+            }
+            return userDTOList.isEmpty() ? Optional.empty() : Optional.of(userDTOList);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        // Eğer Listeleme başarısızsa boş veri dönder
+        return Optional.empty();
     }
 
     @Override
