686f1c3735af26aabdfc5d50e08bc0062ffefb04
berru <berruhanedar@icloud.com>
2025-04-03 02:31:44 +0300

    Implemented UserController with IDaoImplements

--- src/main/java/com/berru/app/atmjfx/controller/UserController.java
+++ src/main/java/com/berru/app/atmjfx/controller/UserController.java
@@ -1,5 +1,56 @@
 package com.berru.app.atmjfx.controller;
 
-public class UserController {
+import com.berru.app.atmjfx.dao.IDaoImplements;
+import com.berru.app.atmjfx.dto.UserDTO;
 
+import java.sql.ResultSet;
+import java.util.List;
+import java.util.Optional;
+
+public class UserController implements IDaoImplements<UserDTO> {
+
+    @Override
+    public Optional<UserDTO> create(UserDTO userDTO) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<List<UserDTO>> list() {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<UserDTO> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<UserDTO> findById(int id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<UserDTO> update(int id, UserDTO userDTO) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<UserDTO> delete(int id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public UserDTO mapToObjectDTO(ResultSet resultSet) {
+        return null;
+    }
+
+    @Override
+    public Optional<UserDTO> selectSingle(String sql, Object... params) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional login(String username, String password) {
+        return Optional.empty();
+    }
 }
