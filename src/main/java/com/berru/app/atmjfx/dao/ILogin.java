5eed1636b40afc15ab03813da37ddbb0d534fd48
berru <berruhanedar@icloud.com>
2025-04-03 02:50:56 +0300

    Rename method to loginUser in ILogin interface

--- src/main/java/com/berru/app/atmjfx/dao/ILogin.java
+++ src/main/java/com/berru/app/atmjfx/dao/ILogin.java
@@ -3,6 +3,5 @@ package com.berru.app.atmjfx.dao;
 import java.util.Optional;
 
 public interface ILogin<T> {
-    Optional<T> login(String username, String password);
-
+    Optional<T> loginUser(String username, String password);
 }
