f36be5366244208e0bc9d4ff2cc05e84a186fc3d
berru <berruhanedar@icloud.com>
2025-04-03 02:51:36 +0300

    Refactor IDaoImplements interface for clarity

--- src/main/java/com/berru/app/atmjfx/dao/IDaoImplements.java
+++ src/main/java/com/berru/app/atmjfx/dao/IDaoImplements.java
@@ -1,34 +1,14 @@
 package com.berru.app.atmjfx.dao;
 
 import com.berru.app.atmjfx.database.SingletonDBConnection;
+import com.berru.app.atmjfx.dto.UserDTO;
+
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.util.List;
 import java.util.Optional;
 
-public interface IDaoImplements<T> extends ILogin{
-    // CREATE
-    Optional<T> create(T t);
-
-    // LIST
-    Optional<List<T>> list();
-
-    // FIND
-    Optional<T> findByName(String name);
-    Optional<T> findById(int id);
-
-    // UPDATE
-    Optional<T> update(int id, T t);
-
-    // DELETE
-    Optional<T> delete(int id);
-
-    // GENERICS METOT (LIST , FIND)
-    T mapToObjectDTO(ResultSet resultSet);
-    Optional<T> selectSingle(String sql , Object... params);
-
-
-    default Connection iDaoImplementsDatabaseConnection(){
+public interface IDaoImplements<T> extends ICrud, IGenericsMethod, ILogin {
+    default Connection iDaoImplementsDatabaseConnection() {
         return SingletonDBConnection.getInstance().getConnection();
     }
+    Optional<UserDTO> update(int id, UserDTO userDTO);
 }
