6c23db2799b611145b7f251d1cb9606c2fd171ea
berru <berruhanedar@icloud.com>
2025-04-04 00:27:51 +0300

    Updated

--- src/main/java/com/berru/app/atmjfx/dao/IDaoImplements.java
+++ src/main/java/com/berru/app/atmjfx/dao/IDaoImplements.java
@@ -6,9 +6,8 @@ import com.berru.app.atmjfx.dto.UserDTO;
 import java.sql.Connection;
 import java.util.Optional;
 
-public interface IDaoImplements<T> extends ICrud, IGenericsMethod, ILogin {
+public interface IDaoImplements<T> extends ICrud<T>,IGenericsMethod<T> {
     default Connection iDaoImplementsDatabaseConnection() {
         return SingletonDBConnection.getInstance().getConnection();
     }
-    Optional<UserDTO> update(int id, UserDTO userDTO);
 }
