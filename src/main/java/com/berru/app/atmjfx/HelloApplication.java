b1d811c86ff3da0430579940db2d5fc2604ebb97
berru <berruhanedar@icloud.com>
2025-04-03 16:47:27 +0300

    Refactored login view initialization

--- src/main/java/com/berru/app/atmjfx/HelloApplication.java
+++ src/main/java/com/berru/app/atmjfx/HelloApplication.java
@@ -2,6 +2,7 @@ package com.berru.app.atmjfx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,10 +11,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("view/login.fxml"));
+        Parent parent = fxmlLoader.load();
+        stage.setTitle("Kullanıcı Yönetimi Login Sayfası");
+        stage.setScene(new Scene(parent));
         stage.show();
     }
 
