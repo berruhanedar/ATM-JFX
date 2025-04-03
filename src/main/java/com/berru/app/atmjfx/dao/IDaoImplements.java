bd43b48a348dc65b666eda7c10b778b20f5e89b1
berru <berruhanedar@icloud.com>
2025-04-04 00:39:19 +0300

    Switch to SingletonPropertiesDBConnection in IDao

--- src/main/java/com/berru/app/atmjfx/dao/IDaoImplements.java
+++ src/main/java/com/berru/app/atmjfx/dao/IDaoImplements.java
@@ -1,13 +1,11 @@
 package com.berru.app.atmjfx.dao;
 
-import com.berru.app.atmjfx.database.SingletonDBConnection;
-import com.berru.app.atmjfx.dto.UserDTO;
+import com.berru.app.atmjfx.database.SingletonPropertiesDBConnection;
 
 import java.sql.Connection;
-import java.util.Optional;
 
-public interface IDaoImplements<T> extends ICrud<T>,IGenericsMethod<T> {
+public interface IDaoImplements<T> extends ICrud<T>, IGenericsMethod<T> {
     default Connection iDaoImplementsDatabaseConnection() {
-        return SingletonDBConnection.getInstance().getConnection();
+        return SingletonPropertiesDBConnection.getInstance().getConnection();
     }
 }
