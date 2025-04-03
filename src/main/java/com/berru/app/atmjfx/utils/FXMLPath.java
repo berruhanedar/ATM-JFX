05ca56d15bf39dd0eef49fbda557186b90a7ea04
berru <berruhanedar@icloud.com>
2025-04-03 16:46:47 +0300

    Added FXML path constants

---
+++ src/main/java/com/berru/app/atmjfx/utils/FXMLPath.java
@@ -0,0 +1,9 @@
+package com.berru.app.atmjfx.utils;
+
+public class FXMLPath {
+    public static final String LOGIN = "/com/berruhanedar/app/atmjfx/view/login.fxml";
+    public static final String REGISTER = "/com/berruhanedar/app/atmjfx/view/register.fxml";
+    public static final String ADMIN = "/com/berruhanedar/app/atmjfx/view/admin.fxml";
+    public static final String USER_HOME = "/com/berruhanedar/app/atmjfx/view/hello-view.fxml";
+}
+
