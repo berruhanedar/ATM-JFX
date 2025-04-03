ffafd01c79e622ae87707b75295140ac0be48d78
berru <berruhanedar@icloud.com>
2025-04-03 16:18:13 +0300

    Updated

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module com.berru.app.atmjfx {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
@@ -10,12 +9,25 @@ module com.berru.app.atmjfx {
     requires org.kordamp.bootstrapfx.core;
     requires static lombok;
     requires java.sql;
-    requires java.desktop;
-    requires org.apache.poi.poi;
-    requires eu.hansolo.tilesfx;
     requires com.h2database;
+    requires jbcrypt;
+    requires org.apache.poi.ooxml;
+    requires org.apache.pdfbox;
+    requires java.desktop;
+    requires java.mail;
 
     opens com.berru.app.atmjfx to javafx.fxml;
     opens com.berru.app.atmjfx.dto to javafx.base, lombok;
+
+    exports com.berru.app.atmjfx.controller to javafx.fxml;
+
+    opens com.berru.app.atmjfx.dao to java.sql;
+    opens com.berru.app.atmjfx.database to java.sql;
+
+    exports com.berru.app.atmjfx.dao;
+    exports com.berru.app.atmjfx.database;
     exports com.berru.app.atmjfx;
+
+    opens com.berru.app.atmjfx.utils to javafx.base, lombok;
+
 }
