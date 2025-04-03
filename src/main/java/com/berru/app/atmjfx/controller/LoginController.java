bbda008a7e667060bd6e454279b1cb21fb7abe7e
berru <berruhanedar@icloud.com>
2025-04-03 15:46:11 +0300

    Added login logic to LoginController

---
+++ src/main/java/com/berru/app/atmjfx/controller/LoginController.java
@@ -0,0 +1,83 @@
+package com.berru.app.atmjfx.controller;
+
+import com.berru.app.atmjfx.dao.UserDAO;
+import com.berru.app.atmjfx.dto.UserDTO;
+import com.berru.app.atmjfx.utils.ERole;
+import com.berru.app.atmjfx.utils.SpecialColor;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.util.Optional;
+
+
+public class LoginController {
+    // Injection
+
+    // Field
+    private UserDAO userDAO;
+
+    // Parameterless Constructor
+    public LoginController() {
+        userDAO = new UserDAO();
+    }
+
+    ///////////////////////////////////////
+    /// FXML Field
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private TextField passwordField;
+
+    private void showAlert(String title, String message, Alert.AlertType type) {
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    @FXML
+    public void login() {
+        String username = this.usernameField.getText().trim();
+        String password = this.passwordField.getText().trim();
+
+        Optional<UserDTO> optionalLoginUserDTO = userDAO.loginUser(username, password);
+
+        if (optionalLoginUserDTO.isPresent()) {
+            UserDTO userDTO = optionalLoginUserDTO.get();
+            showAlert("Success", "Login Successful: " + userDTO.getUsername(), Alert.AlertType.INFORMATION);
+
+            if (userDTO.getRole() == ERole.ADMIN) {
+                openAdminPane();
+            } else {
+                openUserHomePane();
+            }
+        } else {
+            showAlert("Failed", "Login credentials are incorrect", Alert.AlertType.ERROR);
+        }
+    }
+
+    private void openAdminPane() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(FXMLPath.ADMIN));
+            Parent parent = fxmlLoader.load();
+
+            Stage stage = (Stage) usernameField.getScene().getWindow();
+            stage.setScene(new Scene(parent));
+            stage.setTitle("Admin Panel");
+            stage.show();
+        } catch (Exception e) {
+            System.out.println(SpecialColor.RED + "Failed to redirect to the Admin Page" + SpecialColor.RESET);
+            e.printStackTrace();
+            showAlert("Error", "Failed to load the Admin screen", Alert.AlertType.ERROR);
+        }
+
+    }
+
+
+}
